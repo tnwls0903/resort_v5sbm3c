f072918fcfb38afb0cdeab531a1d1177c9c492bd
silversmell <ssm06091@naver.com>
2024-05-03 15:14:52 +0900

    프로젝트 성공을 기원합니다.

---
+++ src/test/java/dev/mvc/team7_v2sbm3c/Team7V2sbm3cApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mvc.team7_v2sbm3c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team7V2sbm3cApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
