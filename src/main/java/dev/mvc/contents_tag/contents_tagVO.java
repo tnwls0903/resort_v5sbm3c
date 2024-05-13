f3181d75c6d7332e8da4d12144b835738abc7738
silversmell <ssm06091@naver.com>
2024-05-13 12:12:05 +0900

    vo 생성

---
+++ src/main/java/dev/mvc/contents_tag/contents_tagVO.java
@@ -0,0 +1,12 @@
+package dev.mvc.contents_tag;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class contents_tagVO {
+	
+	private int contents_tag_no;
+	
+	private int scon_no;
+}
---
+++ src/main/java/dev/mvc/share_image/share_imageVO.java
@@ -0,0 +1,23 @@
+package dev.mvc.share_image;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class share_imageVO {
+	private int file_no;
+	
+	private int scon_no;
+	
+	private String file_origin_name;
+	
+	private String file_upload_name;
+	
+	private String file_thumb_name;
+	
+	private int file_size;
+	
+	private String file_date;
+	
+
+}
